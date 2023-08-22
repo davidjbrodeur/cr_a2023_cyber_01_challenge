fd26aabc6f86c431439938ae7d60f3c00cee5bd6
dbrodeur <davidjbrodeur@gmail.com>
2023-08-22 12:00:29 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    char inputString[100];
+
+    printf("Entrez une chaine de caractères: ");
+    scanf("%s", inputString);
+
+    printf(inputString);
+
+    return 0;
+}
